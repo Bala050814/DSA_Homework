924c05550e52c9c8d822a045eab21b1c2aa9372e
Balamurugan R <balamurugan.r050814@gmail.com>
2026-09-17 09:00:51 +0530

    Implement first unique character finder

---
+++ STRINGS/387_Find_unique_char.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        d={}
+        
+        idx=0
+        for i in s:
+            d[i]=d.get(i,0)+1
+        
+        for i in range(len(s)):
+            if d[s[i]]==1:
+                return i
+                
+        return -1
+        
+        
